456ca207fda2e8987ba270de4b2dc73f778256a6
saochea <saocheaphan@gmail.com>
2023-07-19 11:15:20 +0700

    added new project product api crud operation

---
+++ rest-crud-operation/src/main/java/com/bbc/restcrudoperation/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.bbc.restcrudoperation.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bbc.restcrudoperation.model.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    // You can add custom query methods here if needed
+	Product findByName(String name);
+}
